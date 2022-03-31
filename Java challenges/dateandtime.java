133c83b39d961a49348bac192d27c3bbabb84d1b
ayushi-get <63720826+ayushi-get@users.noreply.github.com>
2022-03-31 19:41:49 +0530

    Add files via upload

---
+++ Java challenges/dateandtime.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class dateandtime {
+    public static void main(String[] args) {
+        Date current_date = new Date();
+        System.out.println(current_date);
+
+    }
+}
